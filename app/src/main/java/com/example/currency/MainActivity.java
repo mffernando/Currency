f5dd7f756f317e1edce1673477e986d2e2ce0a51
Fernando <fmti91@hotmail.com>
2019-04-07 20:27:42 -0300

    Elements

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.currency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder newViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.newViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
+        this.newViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
+        this.newViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
+        this.newViewHolder.buttonCalc = (Button) findViewById(R.id.button_calc);
+
     }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalc;
+    }
+
 }
