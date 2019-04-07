08fe80b805622147acfbb792930c9427ab6766db
Fernando <fmti91@hotmail.com>
2019-04-07 20:48:46 -0300

    Events

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.currency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder newViewHolder = new ViewHolder();
 
@@ -18,8 +19,19 @@ public class MainActivity extends AppCompatActivity {
         this.newViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
         this.newViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
         this.newViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
-        this.newViewHolder.buttonCalc = (Button) findViewById(R.id.button_calc);
 
+        //button
+        this.newViewHolder.buttonCalc = (Button) findViewById(R.id.button_calc);
+        this.newViewHolder.buttonCalc.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_calc) {
+            this.newViewHolder.buttonCalc.setText("TEST");
+        }
     }
 
     private static class ViewHolder {
